11963a2fc5a0a434651c7288a238b43a3bbec13a
Javier Mora <javier.mora@pfsgroups-MacBook-Pro.local>
2021-06-23 13:55:23 +0200

    Modificar error en ejercicios de hooks para el set operation

--- src/components/Hooks/Hooks.js
+++ src/components/Hooks/Hooks.js
@@ -3,28 +3,28 @@ import './Hooks.scss';
 
 const HooksCounter = () => {
     const [count, setCount] = useState(0);
-    let [operation, setOperation] = useState('');
+    const [operation, setOperation] = useState('');
 
     useEffect(() => {
         console.log("Componente renderizado");
-        console.log(count)
+        console.log(count);
         console.log(operation);
     },[operation, count]);
 
     const handleIncrement = (param1, param2) => {
         setCount( count + param1);
-        setOperation(operation = param2)
+        setOperation(param2)
     }
 
     const handleDecrement = (param1, param2) => {
-        setCount( count + param1);
-        setOperation(operation = param2)
+        setCount( count - param1);
+        setOperation(param2)
     }
 
     return (
       <div className="hooks"> 
         <button onClick={() => handleIncrement(1, 'increase')}> + </button>
-        <button onClick={() => handleDecrement(1, 'decrease')}> + </button> 
+        <button onClick={() => handleDecrement(1, 'decrease')}> - </button> 
         <p> Result: {count} </p>
       </div>
     );
